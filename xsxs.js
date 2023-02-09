46f31dfdb08a52dd8ed668ab1ee343d592399b4a
mientospl <56133674+mientospl@users.noreply.github.com>
2023-02-09 18:22:10 +0100

    Add files via upload

---
+++ xsxs.js
@@ -0,0 +1,31 @@
+const liczba1 = parseInt(prompt("wprowadź pierwszą liczbę"))
+
+// alert(liczba1)
+
+// alert("-" + liczba1)
+
+// alert(liczba1 + 15)
+
+// alert(liczba1 + 0.5)
+
+// alert(Math.pow(liczba1, 2))
+
+// alert(liczba1 / 2)
+
+// alert(liczba1 % 2)
+
+// alert(liczba1 + 1)
+
+// alert(liczba1 - 1)
+
+// alert(Math.ceil(liczba1 / 3))
+
+// alert(Math.floor(liczba1 / 3))
+
+// alert(Math.round(liczba1 / 3))
+
+// alert(Math.sqrt(liczba1))
+
+// alert((Math.random() * 50 + 0))
+
+// alert(Math.abs(liczba1))
